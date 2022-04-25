edb00b4627f763d88441916136a81c96a57d458a
Lucas Leonardo <llbs@cin.ufpe.br>
2022-04-25 11:10:10 -0300

    Create a API hello world and healthcheck method

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,19 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+import 'express-async-errors';
+import 'reflect-metadata';
+
+import routes from './routes/routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.get("/healthcheck", (request: Request, response: Response) => {
+    response.send("ok").status(200)
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333');
+});
