537af9a90463d8b8cd4fd51571fee8a9f1b8f5d5
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:46:46 -0300

    Add a delete method in interface

--- src/modules/patient/repositories/IPatientsRepository.ts
+++ src/modules/patient/repositories/IPatientsRepository.ts
@@ -1,9 +1,12 @@
 import Patient from '../infra/typeorm/entities/Patient';
 
 import ICreatePatientDTO from '../dtos/ICreatePatientDTO';
+import IUpdatePatientDTO from '../dtos/IUpdatePatientDTO';
+import IDeletePatientDTO from '../dtos/IDeletePatientDTO';
 
 interface IPatientsRepository {
     create(data: ICreatePatientDTO): Promise<Patient>;
+    delete(data: IDeletePatientDTO): Promise<void>
 }
 
 export default IPatientsRepository;
