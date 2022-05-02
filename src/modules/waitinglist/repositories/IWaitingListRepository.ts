583f2acd2c9ca5f6aa4a7b2047cb3e5f2d4d607a
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 12:57:24 -0300

    Update sign of removePatient method in repository interface

--- src/modules/waitinglist/repositories/IWaitingListRepository.ts
+++ src/modules/waitinglist/repositories/IWaitingListRepository.ts
@@ -2,10 +2,11 @@ import WaitingList from '../infra/typeorm/entities/WaitingList';
 
 import IAddPatientInListDTO from '../dtos/IAddPatientInListDTO';
 import IAttendPatientDTO from '../dtos/IAttendPatientDTO';
+import IRemovePatientFromListDTO from '../dtos/IRemovePatientFromListDTO';
 
 interface IWaitingListRepository {
     addPatient(data: IAddPatientInListDTO): Promise<WaitingList>;
-    removePatient(): void;
+    removePatient(data: IRemovePatientFromListDTO): Promise<void>;
     attendPatient(data: IAttendPatientDTO): Promise<WaitingList>;
     findAll(): Promise<WaitingList[]>;
     findByCpf(cpf: string): Promise<WaitingList | undefined>
