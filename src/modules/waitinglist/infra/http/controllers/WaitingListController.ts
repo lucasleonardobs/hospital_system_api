8141573256bcb8afb78a12f9bfa023d01160be24
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 13:00:33 -0300

    Add a route to removePatient

--- src/modules/waitinglist/infra/http/controllers/WaitingListController.ts
+++ src/modules/waitinglist/infra/http/controllers/WaitingListController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express';
 
 import AddPatientService from '../../../services/AddPatientService';
 import AttendPatientService from '../../../services/AttendPatientService';
+import RemovePatientService from '../../../services/RemovePatientService';
 
 class WaitingListController {
     public async addPatient(request: Request, response: Response): Promise<Response> {
@@ -20,7 +21,15 @@ class WaitingListController {
     };
 
     public async removePatient(request: Request, response: Response): Promise<Response> {
-        return response.json();
+        const { cpf } = request.params;
+
+        const deletePatient = container.resolve(RemovePatientService);
+
+        await deletePatient.execute({ cpf });
+
+        return response.json({
+            message: 'Patient has been remove from waiting list with successful.',
+        });
     };
 
     public async attendPatient(request: Request, response: Response): Promise<Response> {
