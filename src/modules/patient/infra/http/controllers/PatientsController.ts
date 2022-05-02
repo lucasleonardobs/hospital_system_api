206d009c3542380d00e13b49c40177a92e48cb54
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:47:15 -0300

    Add a delete method in Controller

--- src/modules/patient/infra/http/controllers/PatientsController.ts
+++ src/modules/patient/infra/http/controllers/PatientsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreatePatientService from '../../../services/CreatePatientService';
+import DeletePatientService from '../../../services/DeletePatientService';
 
 class PatientsController {
     public async create(request: Request, response: Response): Promise<Response> {
@@ -21,6 +22,19 @@ class PatientsController {
 
         return response.json(patient)
     };
+
+    public async delete(request: Request, response: Response): Promise<Response> {
+        const { cpf } = request.params;
+
+        const deletePatient = container.resolve(DeletePatientService);
+
+        await deletePatient.execute({ cpf });
+
+        return response.json({
+            message: 'Order has been deleted with successful.',
+        });
+    }
+
 };
 
 export default PatientsController;
