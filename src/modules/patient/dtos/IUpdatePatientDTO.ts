811eea16a817ce16657bba3b86ddda60be7377b6
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:52:27 -0300

    Add a DTO to update Patient

---
+++ src/modules/patient/dtos/IUpdatePatientDTO.ts
@@ -0,0 +1,10 @@
+interface IUpdatePatientDTO {
+    name: string;
+    date_of_birth: Date;
+    phone_number: string;
+    cpf: string;
+    cep: string;
+    address: string;
+}
+    
+export default IUpdatePatientDTO;
