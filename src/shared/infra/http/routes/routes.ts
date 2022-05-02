b4e646a217153bd6205c861206328d7c1fd778a4
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:31:56 -0300

    Add patientsRouter in main routes

--- src/shared/infra/http/routes/routes.ts
+++ src/shared/infra/http/routes/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import patientsRouter from '../../../../modules/patient/infra/http/routes/Patients.routes'
+
 const routes = Router();
+
+routes.use('/patients', patientsRouter)
 
 export default routes;
