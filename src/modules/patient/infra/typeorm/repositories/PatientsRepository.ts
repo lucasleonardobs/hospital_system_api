54f6d5dc5dba301d0ab5fbbc7458a563bb5e5796
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:57:07 -0300

    Add a findByCpf and update methods in patient repository

--- src/modules/patient/infra/typeorm/repositories/PatientsRepository.ts
+++ src/modules/patient/infra/typeorm/repositories/PatientsRepository.ts
@@ -2,9 +2,12 @@
 import { getRepository, Repository } from 'typeorm';
 
 import ICreatePatientDTO from '../../../dtos/ICreatePatientDTO';
-
+import IUpdatePatientDTO from '../../../dtos/IUpdatePatientDTO';
 import IDeletePatientDTO from '../../../dtos/IDeletePatientDTO';
 import IPatientsRepository from '../../../repositories/IPatientsRepository';
+
+
+import AppError from '../../../../../shared/errors/AppError';
 
 import Patient from '../entities/Patient';
 
@@ -37,6 +40,43 @@ class PatientsRepository implements IPatientsRepository {
 
     public async delete({ cpf }: IDeletePatientDTO): Promise<void> {
         await this.ormRepository.delete(cpf);
+    }
+
+    public async update({
+        name,
+        date_of_birth,
+        cpf,
+        cep,
+        address,
+    }: IUpdatePatientDTO): Promise<Patient> {
+        const patient = await this.ormRepository.findOne({
+            cpf
+        });
+
+        if (!patient) {
+            throw new AppError("Patient not found", 404);
+        };
+
+        const updatedPatient = {
+            ...patient,
+            name,
+            date_of_birth,
+            cpf,
+            cep,
+            address,
+        };
+
+        await this.ormRepository.save(updatedPatient);
+
+        return updatedPatient;
+    }
+
+    public async findByCpf(cpf: string): Promise<Patient | undefined> {
+        const user = await this.ormRepository.findOne({
+            where: { cpf },
+        });
+
+        return user;
     }
 
 }
