efb536cf00fefb226247a2f7dff08fbf9e51d6b8
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 11:49:33 -0300

    Create WaitingList model and relation with Patient

---
+++ src/modules/waitinglist/infra/typeorm/entities/WaitingList.ts
@@ -0,0 +1,39 @@
+import Patient from '../../../../patient/infra/typeorm/entities/Patient';
+
+import {
+    Entity,
+    Column,
+    PrimaryColumn,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    OneToOne,
+    JoinColumn,
+} from 'typeorm';
+
+@Entity('waitinglists')
+class WaitingList {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    patient_cpf: string;
+
+    @OneToOne(() => Patient)
+    @JoinColumn({ name: "patient_cpf" })
+    patient: Patient;
+
+    @Column()
+    attended: boolean;
+
+    @Column()
+    priority: 0 | 1 | 2;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default WaitingList;
