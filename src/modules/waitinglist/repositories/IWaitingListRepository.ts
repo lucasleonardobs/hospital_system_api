d1a32bc879fa5f0f4cd809e4f07d196b5489b04b
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 12:41:45 -0300

    Update sign of attendPatient in repository interface

--- src/modules/waitinglist/repositories/IWaitingListRepository.ts
+++ src/modules/waitinglist/repositories/IWaitingListRepository.ts
@@ -1,11 +1,12 @@
 import WaitingList from '../infra/typeorm/entities/WaitingList';
 
 import IAddPatientInListDTO from '../dtos/IAddPatientInListDTO';
+import IAttendPatientDTO from '../../../dtos/IAttendPatientDTO';
 
 interface IWaitingListRepository {
     addPatient(data: IAddPatientInListDTO): Promise<WaitingList>;
     removePatient(): void;
-    attendPatient(): Promise<WaitingList>;
+    attendPatient(data: IAttendPatientDTO): Promise<WaitingList>;
     findAll(): Promise<WaitingList[]>;
     findByCpf(cpf: string): Promise<WaitingList | undefined>
 }
