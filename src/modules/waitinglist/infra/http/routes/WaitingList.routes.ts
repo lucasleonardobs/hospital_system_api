1e227c30daec7201d701e7ddf74aded5c7c8b5ec
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 12:11:50 -0300

    Create a routes having all endpoints

--- src/modules/waitinglist/infra/http/routes/WaitingList.routes.ts
+++ src/modules/waitinglist/infra/http/routes/WaitingList.routes.ts
@@ -5,11 +5,10 @@ import WaitingListController from '../controllers/WaitingListController';
 const waitingListRouter = Router();
 const waitingListController = new WaitingListController();
 
-waitingListRouter.post('/', waitingListController.create);
-// waitingListRouter.get('/', waitingListController.index)
-// waitingListRouter.get('/:cpf', waitingListController.show);
-// waitingListRouter.post('/', waitingListController.create);
-// waitingListRouter.put('/:cpf', waitingListController.update);
-// waitingListRouter.delete('/:cpf', waitingListController.delete);
+waitingListRouter.post('/', waitingListController.addPatient);
+waitingListRouter.put('/:cpf', waitingListController.attendPatient);
+waitingListRouter.delete('/:cpf', waitingListController.removePatient);
+waitingListRouter.get('/', waitingListController.index)
+waitingListRouter.get('/:cpf', waitingListController.show);
 
 export default waitingListRouter;
