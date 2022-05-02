b61a4e1860a7e9ae9575fef92e75dd33be1b66e6
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 13:42:16 -0300

    Use listWaitingListValidator as middleware in route

--- src/modules/waitinglist/infra/http/routes/WaitingList.routes.ts
+++ src/modules/waitinglist/infra/http/routes/WaitingList.routes.ts
@@ -4,6 +4,7 @@ import addPatientValidator from '../validators/AddPatientValidator';
 import attendPatientValidator from '../validators/AttendPatientValidator';
 import removePatientValidator from '../validators/RemovePatientValidator';
 import showAPatientInListValidator from '../validators/ShowAPatientInListValidator';
+import listWaitingListValidator from '../validators/ListWaitingListValidator';
 
 import WaitingListController from '../controllers/WaitingListController';
 
@@ -13,7 +14,7 @@ const waitingListController = new WaitingListController();
 waitingListRouter.post('/', addPatientValidator, waitingListController.addPatient);
 waitingListRouter.patch('/:cpf', attendPatientValidator, waitingListController.attendPatient);
 waitingListRouter.delete('/:cpf', removePatientValidator, waitingListController.removePatient);
-waitingListRouter.get('/', waitingListController.index);
+waitingListRouter.get('/', listWaitingListValidator, waitingListController.index);
 waitingListRouter.get('/:cpf', showAPatientInListValidator, waitingListController.show);
 
 export default waitingListRouter;
