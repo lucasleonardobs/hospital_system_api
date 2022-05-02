94c7be107f0a9769c02a104efe059b69f2070cc9
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:56:16 -0300

    Add a findByCpf and Update methods in mock repository

--- src/modules/patient/repositories/fakes/FakePatientsRepository.ts
+++ src/modules/patient/repositories/fakes/FakePatientsRepository.ts
@@ -1,9 +1,13 @@
 import ICreatePatientDTO from '../../dtos/ICreatePatientDTO';
+import IUpdatePatientDTO from '../../dtos/IUpdatePatientDTO';
 import IDeletePatientDTO from '../../dtos/IDeletePatientDTO';
 
 import IPatientsRepository from '../IPatientsRepository';
 
 import Patient from '../../infra/typeorm/entities/Patient';
+
+import AppError from '../../../../shared/errors/AppError';
+
 class PatientsRepository implements IPatientsRepository {
     private patients: Patient[] = [];
 
@@ -30,6 +34,36 @@ class PatientsRepository implements IPatientsRepository {
         this.patients.push(patient);
 
         return patient;
+    }
+
+    public async update({
+        name,
+        date_of_birth,
+        cpf,
+        cep,
+        address,
+        phone_number
+    }: IUpdatePatientDTO): Promise<Patient> {
+        const findPatient = await this.patients.find(patient => patient.cpf === cpf);
+
+        if (!findPatient) {
+            throw new AppError("Patient not found", 404);
+        };
+
+        findPatient.name = name;
+        findPatient.date_of_birth = date_of_birth;
+        findPatient.cpf = cpf;
+        findPatient.cep = cep;
+        findPatient.address = address;
+        findPatient.phone_number = phone_number;
+
+        return findPatient;
+    }
+    
+    public async findByCpf(cpf: string): Promise<Patient | undefined> {
+        const findPatient = this.patients.find(patient => patient.cpf === cpf);
+
+        return findPatient;
     }
 
     public async delete({ cpf }: IDeletePatientDTO): Promise<void> {
