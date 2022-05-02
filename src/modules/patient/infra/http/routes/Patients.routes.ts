4e7e702afc0abf157ef622e06827a8e4b4f31bcc
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:50:03 -0300

    Add a update route

--- src/modules/patient/infra/http/routes/Patients.routes.ts
+++ src/modules/patient/infra/http/routes/Patients.routes.ts
@@ -4,17 +4,13 @@ import PatientController from '../controllers/PatientsController';
 
 import createPatientValidator from '../validators/CreatePatientValidator';
 import updatePatientValidator from '../validators/UpdatePatientValidator';
-import showPatientValidator from '../validators/ShowPatientValidator';
 import deletePatientValidator from '../validators/DeletePatientValidator';
-import paginatePatientValidator from '../validators/PaginatePatientValidator';
 
 const patientRouter = Router();
 const patientController = new PatientController();
 
 patientRouter.post('/', createPatientValidator, patientController.create);
-patientRouter.get('/', paginatePatientValidator, patientController.index)
-patientRouter.get('/:cpf', showPatientValidator, patientController.show);
-patientRouter.put('/:cpf', updatePatientValidator, patientController.update);
 patientRouter.delete('/:cpf', deletePatientValidator, patientController.delete);
+patientRouter.put('/:cpf', updatePatientValidator, patientController.update);
 
 export default patientRouter;
