7cf1ff3312efd0be35fecc8f5dc44fcc38e8cea2
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 12:30:35 -0300

    Add addpatient validator as middleware in route

--- src/modules/waitinglist/infra/http/routes/WaitingList.routes.ts
+++ src/modules/waitinglist/infra/http/routes/WaitingList.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import addPatientValidator from '../validators/AddPatientValidator';
+
 import WaitingListController from '../controllers/WaitingListController';
 
 const waitingListRouter = Router();
 const waitingListController = new WaitingListController();
 
-waitingListRouter.post('/', waitingListController.addPatient);
+waitingListRouter.post('/', addPatientValidator, waitingListController.addPatient);
 waitingListRouter.put('/:cpf', waitingListController.attendPatient);
 waitingListRouter.delete('/:cpf', waitingListController.removePatient);
 waitingListRouter.get('/', waitingListController.index)
