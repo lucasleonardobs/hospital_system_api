7978f381e2c06ed53c167d39323e29b487462223
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 12:19:46 -0300

    Add WaitingListRepository by dependency injection.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IPatientsRepository from '../../modules/patient/repositories/IPatientsRepository';
 import PatientsRepository from '../../modules/patient/infra/typeorm/repositories/PatientsRepository';
 
+import IWaitingListRepository from '../../modules/waitinglist/repositories/IWaitingListRepository';
+import WaitingListRepository from '../../modules/waitinglist/infra/typeorm/repositories/WaitingListRepository';
+
 container.registerSingleton<IPatientsRepository>(
     'PatientsRepository',
     PatientsRepository,
+);
+
+container.registerSingleton<IWaitingListRepository>(
+    'WaitingListRepository',
+    WaitingListRepository,
 );
