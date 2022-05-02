2998a045530476c3ad2a7e03b5f6e5b90661f661
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 13:06:50 -0300

    Create middleware to handle response when AppError is instanced.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,14 +1,34 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import { errors } from 'celebrate';
 
 import 'express-async-errors';
+import '../typeorm/index';
 
 import routes from './routes/routes';
-import '../typeorm/index';
+import AppError from '../../errors/AppError';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+  
+    console.error(err);
+  
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
+
+app.use(errors());
 
 app.get("/healthcheck", (_: Request, response: Response) => {
     response.send("ok").status(200)
