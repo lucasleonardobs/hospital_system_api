ce9b3f725ec5fe57f9defde0d66d715d93aa5119
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 10:50:14 -0300

    Add a update function in controller

--- src/modules/patient/infra/http/controllers/PatientsController.ts
+++ src/modules/patient/infra/http/controllers/PatientsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreatePatientService from '../../../services/CreatePatientService';
+import UpdatePatientService from '../../../services/UpdatePatientService';
 import DeletePatientService from '../../../services/DeletePatientService';
 
 class PatientsController {
@@ -34,6 +35,24 @@ class PatientsController {
             message: 'Order has been deleted with successful.',
         });
     }
+
+    public async update(request: Request, response: Response): Promise<Response> {
+        const { cpf } = request.params;
+        const { name, date_of_birth, phone_number, cep, address } = request.body;
+
+        const updatePatient = container.resolve(UpdatePatientService);
+
+        const updatedPatient = updatePatient.execute({
+            cpf,
+            name,
+            date_of_birth,
+            phone_number,
+            cep,
+            address
+        });
+
+        return response.json(updatedPatient);
+    };
 
 };
 
