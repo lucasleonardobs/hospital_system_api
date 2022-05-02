aa1052b470b518d253700bef16553de46203cf80
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-02 18:48:11 -0300

    Add middleware to handle AppError and send custom response

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -11,6 +11,24 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errors()); 
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+        });
+    }
+  
+    console.error(err);
+  
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
+
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
