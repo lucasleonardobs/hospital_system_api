346417496d5077fe52af03a32305b3b6652283a4
Lucas Leonardo <llbs@cin.ufpe.br>
2022-05-03 17:46:09 -0300

    Add cors() in express

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { errors } from 'celebrate';
+import cors from 'cors';
 
 import 'express-async-errors';
 import 'reflect-metadata';
@@ -11,8 +12,8 @@ import AppError from '../../errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(routes);
 app.use(errors()); 
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -22,14 +23,16 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
             message: err.message,
         });
     }
-  
+    
     console.error(err);
-  
+    
     return response.status(500).json({
         status: 'error',
         message: 'Internal server error',
     });
 });
+
+app.use(routes);
 
 app.get("/healthcheck", (_: Request, response: Response) => {
     response.send("ok").status(200)
